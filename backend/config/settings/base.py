45aae018e65ee079a78990beae74c0121cc80aaf
Slavi Dimitrov <slavidimitrov54@gmail.com>
2026-08-02 17:01:28 +0300

    add more db configuration for deployment

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -101,6 +101,8 @@ DATABASES = {
         "PASSWORD": env("DB_PASSWORD"),
         "HOST": env("DB_HOST", default="localhost"),
         "PORT": env("DB_PORT", default="5432"),
+        "CONN_MAX_AGE": env("DB_CONN_MAX_AGE", default=60, cast=int),
+        "CONN_HEALTH_CHECKS": True,
     }
 }
 
